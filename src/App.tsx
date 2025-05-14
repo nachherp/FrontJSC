dcfd87e861ff1f0f6b317a27371f0bddf667d340
IRD22-JIHP <ih43108@gmail.com>
2025-05-14 12:07:20 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-800">
+        Tailwind está funcionando 🎉
+      </h1>
+    </div>
+  );
+}
