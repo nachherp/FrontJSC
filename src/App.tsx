34c3134396647ad5d1c1bff8a05138dd1d0553f9
Leticia565 <23394102@utcancun.edu.mx>
2025-05-16 02:04:15 -0500

    modificaciones de la pagina principal uy header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-export default function App() {
-  return (
-    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-800">
-        Tailwind está funcionando 🎉
-      </h1>
-    </div>
-  );
+import Principal from "./features/Principal"
+import "./App.css"
+
+function App() {
+  return <Principal />
 }
+
+export default App
